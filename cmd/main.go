4b7c5d40dda9923179426c15899a0ee14e30b8f8
yuuumiravy <hikoki.y@gmail.com>
2023-07-27 17:48:31 +0900

    feat: setup environment

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello, World!")
+	})
+	serverPORT := os.Getenv("PORT")
+	e.Logger.Fatal(e.Start(":" + serverPORT))
+}
